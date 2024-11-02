a55f9855449eaafacbb479da1f0252e8b7841121
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2024-11-02 21:55:11 +0530

    afterclas

--- afterclass.py
+++ afterclass.py
@@ -7,9 +7,10 @@ sum = 0
 temp = num
 while temp > 0:
     digit = temp % 10
-    sum += digit ** 4
+    sum += digit ** 3
     temp //= 10
-    if num == sum:
+
+if num == sum:
         print(num,"is an armstrong number")
 else:
     print(num,"is not an armstrong number")
